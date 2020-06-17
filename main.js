40d6098069d880bcc9c35310fcbcfe05882b9bba
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-17 11:31:32 -0700

    lab

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 // Your code here!
 
+const colorCombinator = require("./color-combinator");
+const colorDeconstructor = require("./color-deconstructor");
+
+const colorOne = getInput(0);
+const colorTwo = getInput(1);
+
+const combineColors = colorCombinator(colorOne, colorTwo);
+const deconstructColor = colorDeconstructor(colorOne);
+
+if (colorOne === undefined && colorTwo === undefined) {
+  console.log('You need to enter a color!');
+} 
+if (colorOne !== undefined && colorTwo === undefined) {
+  console.log(deconstructColor);
+}
+if (colorOne !== undefined && colorTwo !== undefined) {
+  console.log(combineColors);
+}
+
+
+
+
+
+
+
+
+
 
 
 // Our helper function here! Don't  touch!
